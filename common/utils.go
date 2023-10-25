bb110e117873294986e5d73002e6c71d9dd28f93
prakharagarwal1 <prakharagarwal3031@gmail.com>
2023-10-25 19:39:50 +0530

    feat: add command graph

--- common/utils.go
+++ common/utils.go
@@ -905,3 +905,14 @@ func JsonifyMapValues(inputMap map[string]interface{}) map[string]interface{} {
 	}
 	return inputMap
 }
+
+// FindIndex returns the index of the first element that satisfies the condition.
+// It returns -1 if none of the elements satisfy the condition.
+func FindIndex[T interface{}](vs []T, condition func(T) bool) int {
+	for i, v := range vs {
+		if condition(v) {
+			return i
+		}
+	}
+	return -1
+}
